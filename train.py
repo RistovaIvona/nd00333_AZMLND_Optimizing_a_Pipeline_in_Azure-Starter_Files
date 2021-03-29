e27a9a6cb9939163fc96b478ea6b81628b9a76b5
Ivona Ristova <ivona.feit@gmail.com>
2021-03-29 12:33:47 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -10,17 +10,10 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
 ds = TabularDatasetFactory.from_delimited_files(path='https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv')
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-#Split data into train and test sets.
-x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.33)
 
 run = Run.get_context()
 
@@ -49,6 +42,13 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+    return x_df, y_df
+
+# Drop NAs and encode data.
+x, y = clean_data(ds)
+
+#Split data into train and test sets.
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.33)
     
 
 def main():
